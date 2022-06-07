f67da67d9b83ebd1c4f170dc3bf3e6b44b5f0a92
Francisco C <franck_021097@hotmail.com>
2022-06-06 19:06:17 -0500

    menu visible

--- docs/acciones.js
+++ docs/acciones.js
@@ -1,3 +1,15 @@
-function ver(){
-    document.getElementsByClassName(".educacion").style.display = "block";
+function ver(ver){
+var secciones=["experiencia", "objetivo","experiencia","interes","educacion","Información_personal","idiomas",
+"aptitudes","proyectos"];
+				  
+    
+    for(let i of secciones){
+        if(ver!==i){
+            console.log("if\n el valor de i es: "+i);
+            var ocultar=document.getElementById(i).style.display="none";
+        }else{
+            var ocultar=document.getElementById(i).style.display="block";
+            console.log("else "+i);
+        }
+    }
 }
