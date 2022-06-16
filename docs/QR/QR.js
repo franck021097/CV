d07b4c12133bc87b731a80cd8c5ee74b4aca14cb
Francisco C <franck_021097@hotmail.com>
2022-06-15 21:23:48 -0500

    QR y random certificados

---
+++ docs/QR/QR.js
@@ -0,0 +1,3 @@
+const contenedor = document.getElementById("Codigo");
+
+new QRCode(contenedor, 'https://franck021097.github.io/');
--- docs/acciones.js
+++ docs/acciones.js
@@ -1,15 +1,45 @@
+function usuario(){
+    var i= document.getElementById("preload").style.display="none";
+    i= document.getElementById("header").style.display="block";
+    i= document.getElementById("main").style.display="block";
+    i= document.getElementById("footer").style.display="grid";
+}
+
+
 function ver(ver){
+
 var secciones=["experiencia", "objetivo","experiencia","interes","educacion","Información_personal","idiomas",
 "aptitudes","proyectos"];
-				  
+
+var imagenes=["1","2","3"];
+
+var nombres_certificados=[
+    "imagenes/ipnFPC.jpeg","imagenes/cvCarso.png","imagenes/rCelTab.png","imagenes/fpMiriada.png","imagenes/ipMiX.png","imagenes/pbPlatzi.png","imagenes/ccbPlatzi.png","imagenes/cfeyePlatzi.png","imagenes/cdhtml-cssPlatzi.png","imagenes/cphtml-cssPlatzi.png","imagenes/cssiPlatzi.png","imagenes/cetebPlatzi.png","imagenes/tipPlatzi.png","imagenes/cipPlatzi.png","imagenes/ciba1psPlatzi.png","imagenes/cpreworkPlatzi.png","imagenes/crdmfPlatzi.png","imagenes/cssSL.jpg","imagenes/htmlSL.jpg","imagenes/jsSL.jpg","imagenes/c++SL.jpg","imagenes/sqlSL.jpg","imagenes/excel.jpg","imagenes/word.png"];
+
+
+let indice=[];
+
+for(let i=0; i<3; i++){
+    indice[i] = Math.floor(Math.random()*(23));
+}
+
+console.log("los valores son: " +"\n"+indice[0]+"\n"+ indice[1] +"\n"+indice[2]);
+
+
+
+for (let i=0; i<3; i++) {
+    var valor =document.getElementById(imagenes[i]);
+    valor.setAttribute("src", nombres_certificados[indice[i]]);
     
-    for(let i of secciones){
+}
+
+
+for(let i of secciones){
         if(ver!==i){
-            console.log("if\n el valor de i es: "+i);
             var ocultar=document.getElementById(i).style.display="none";
         }else{
             var ocultar=document.getElementById(i).style.display="block";
-            console.log("else "+i);
         }
     }
 }
+
