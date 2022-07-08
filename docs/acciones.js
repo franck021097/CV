690a27ecd1b48815e670b2c728cbe61f08d1cfb3
Francisco C <franck_021097@hotmail.com>
2022-07-07 20:57:23 -0500

    Verison 2.0.0.0

--- docs/acciones.js
+++ docs/acciones.js
@@ -1,91 +1,12 @@
-function preload(){
-    ocultar();
-    var i= document.getElementById("opciones").style.display="none";
-    i= document.getElementById("preload").style.display="grid";
-setTimeout(
-function(){
-    var i= document.getElementById("preload-p");
-    i.innerHTML="Esto no es solo una pagina"
-},3000);
-
-
-setTimeout(
-function(){
-    var i= document.getElementById("preload-p");
-    i.innerHTML="Es una experiencia";
-},6000);
-
-setTimeout(
-function(){
-    var i= document.getElementById("preload-p");
-    i.innerHTML="";
-},9000);
-
-setTimeout(
-function(){
-    var im= document.getElementById("flecha");
-    im.setAttribute("onclick","usuario()");
-    im.setAttribute("src","imagenes/Flecha.png");
-},10000);
-
+function verMenu(){
+    var ver = document.querySelector(".nav__opciones").style.display="grid";
+    var abrir = document.querySelector(".nav__iconoMenu-logo1").style.display="none";
+    var cerrar = document.querySelector(".nav__iconoMenu-logo2").style.display="block";
+    asignar();
 }
 
-function usuario(){
-    var i= document.getElementById("preload").style.display="none";
-    i= document.getElementById("opciones").style.display="none";
-    i= document.getElementById("header").style.display="grid";
-    i= document.getElementById("presentacion").style.display="grid";
-    i= document.getElementById("main").style.display="block";
-    i= document.getElementById("footer").style.display="grid";
-    var im= document.getElementById("flecha").style.display="none";
-    var i= document.getElementById("prueba").style.display="grid";
+function ocultarMenu(){
+    var ver = document.querySelector(".nav__opciones").style.display="none";
+    var abrir = document.querySelector(".nav__iconoMenu-logo1").style.display="block";
+    var cerrar = document.querySelector(".nav__iconoMenu-logo2").style.display="none";
 }
-
-function ocultar(){
-    var i= document.getElementById("preload").style.display="none";
-    i= document.getElementById("header").style.display="none";
-    i= document.getElementById("presentacion").style.display="none";
-    i= document.getElementById("main").style.display="none";
-    i= document.getElementById("footer").style.display="none";
-    i= document.getElementById("prueba").style.display="none";
-    i= document.getElementById("opciones").style.display="grid";
-}
-
-function ver(ver){
-
-var i= document.getElementById("prueba").style.display="grid";
-    
-var secciones=["experiencia", "objetivo","experiencia","interes","educacion","Información_personal","idiomas",
-"aptitudes","proyectos"];
-
-var imagenes=["1","2","3","4","5"];
-
-var nombres_certificados=[
-    "imagenes/ipnFPC.jpeg","imagenes/cvCarso.png","imagenes/rCelTab.png","imagenes/fpMiriada.png","imagenes/ipMiX.png","imagenes/pbPlatzi.png","imagenes/ccbPlatzi.png","imagenes/cfeyePlatzi.png","imagenes/cdhtml-cssPlatzi.png","imagenes/cphtml-cssPlatzi.png","imagenes/cssiPlatzi.png","imagenes/cetebPlatzi.png","imagenes/tipPlatzi.png","imagenes/cipPlatzi.png","imagenes/ciba1psPlatzi.png","imagenes/cpreworkPlatzi.png","imagenes/crdmfPlatzi.png","imagenes/cssSL.jpg","imagenes/htmlSL.jpg","imagenes/jsSL.jpg","imagenes/c++SL.jpg","imagenes/sqlSL.jpg","imagenes/excel.jpg","imagenes/word.png"];
-
-
-let indice=[];
-
-for(let i=0; i<imagenes.length; i++){
-    indice[i] = Math.floor(Math.random()*(23));
-}
-
-console.log("los valores son: " +"\n"+indice[0]+"\n"+ indice[1] +"\n"+indice[2]);
-
-
-for (let i=0; i<imagenes.length; i++) {
-    var valor =document.getElementById(imagenes[i]);
-    valor.setAttribute("src", nombres_certificados[indice[i]]);
-    
-}
-
-
-for(let i of secciones){
-        if(ver!==i){
-            var ocultar=document.getElementById(i).style.display="none";
-        }else{
-            var ocultar=document.getElementById(i).style.display="block";
-        }
-    }
-}
-
