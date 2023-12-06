2ac9415658d7b5ed31e497edfb9b171358ef3714
Francisco Castro <francisco.castro@ariadnacg.com>
2023-12-06 12:09:58 -0600

    ajuste en vue1.1

--- docs/vue/vue.config.js
+++ docs/vue/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue/dist/' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
+    ? '/vue/' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
     : '/'
 })
 
