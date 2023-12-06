801db62590176cc33519cdd69f54faa786bfe259
Francisco Castro <francisco.castro@ariadnacg.com>
2023-12-06 10:57:59 -0600

    ajuste en vue

--- docs/vue/vue.config.js
+++ docs/vue/vue.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://franck021097.github.io/' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
+    ? 'https://franck021097.github.io/vue/dist/index.html' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
     : '/'
 }
