6a0de90172bd1089f194d08b7dfae8c35a3eb089
Francisco Castro <francisco.castro@ariadnacg.com>
2023-12-06 12:06:11 -0600

    ajuste en vue4

--- docs/vue/vue.config.js
+++ docs/vue/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://franck021097.github.io/vue/dist/' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
+    ? '/vue/dist/' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
     : '/'
 })
 
