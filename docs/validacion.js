1c652c44786fc3620115acda8b1ad7904589d54a
Francisco Castro <franck_021097@hotmail.com>
2024-05-22 20:58:13 -0600

    ligas actualizadas

--- docs/validacion.js
+++ docs/validacion.js
@@ -1,1 +1,9 @@
 //Haz tú validación en javascript acá
+
+const submit = document.querySelector(".formcontato__botao");
+
+submit.addEventListener('click', ()=>{
+    // const nota = document.querySelector('#nota');
+    nota.classList.add("nota_activa");
+    // document.querySelectorAll('.formcontato__subtext')[2].classList.add("nota_activa");
+})
