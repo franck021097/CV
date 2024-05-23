481ba87f30dd06194b2af12bc6dbffd3bcafcaa9
Francisco Castro <franck_021097@hotmail.com>
2024-05-22 21:11:53 -0600

    ignore git

--- docs/vue/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue/' // Cambia 'nombre-repositorio' por el nombre de tu repositorio
-    : '/'
-})
-
