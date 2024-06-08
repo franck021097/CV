517ca52cbb963a89c14d0142a9dd678373a8b446
Francisco Castro <franck_021097@hotmail.com>
2024-06-07 19:40:23 -0600

    api remota

--- docs/validacion.js
+++ docs/validacion.js
@@ -1,9 +1,49 @@
-//Haz tú validación en javascript acá
-
 const submit = document.querySelector(".formcontato__botao");
 
-submit.addEventListener('click', ()=>{
-    // const nota = document.querySelector('#nota');
+submit.addEventListener('click', (evento)=>{
+    
     nota.classList.add("nota_activa");
-    // document.querySelectorAll('.formcontato__subtext')[2].classList.add("nota_activa");
+    obtenerRegistro(evento);
 })
+
+
+async function obtenerRegistro(evento){
+
+    evento.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const asunto = document.querySelector("#asunto").value;
+    const mensaje = document.querySelector("#mensaje").value;
+
+    const fecha = new Date();
+
+    try{
+        const respuesta = await enviarFormulario(nombre,email,asunto,mensaje,fecha);
+
+        if(respuesta.ok) console.log(respuesta.ok)
+    }catch(e){
+        console.log(e);
+    }
+
+    
+}
+
+
+async function enviarFormulario(nombre,email,asunto,mensaje,fecha){
+    const conexion= await fetch("https://apiportafolio.vercel.app/registros",{
+    method:"POST",
+    headers:{
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        nombre: nombre,
+        email: email,
+        asunto: asunto,
+        mensaje: mensaje,
+        fecha: fecha
+    }),
+    });
+
+    return conexion;
+
+}
